732fbb851bb4aa9d93199b0f33455697c7beba09
NightlyD3V <TylerSpaulding95@gmail.com>
2019-12-30 23:28:12 +0000

    Added gifs

--- users/user-router.js
+++ users/user-router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const User = require('./user-model');
+const Users = require('./user-model');
 //BCRYPT
 const bcrypt = require('bcryptjs');
 //MIDDLEWARE
@@ -7,35 +7,59 @@ const  ValidateUser  = require('../middleware/custom');
 const router = express.Router();
 
 /* api/users/ */
-
-router.post('/login', ValidateUser, async (req, res) => {
-    const credentials = { body } = req;
-    try {
-        const user = await User.login(body);
-        if (!user || !bcrypt.compareSync(credentials.password, user.password)) {
-            return res.status(401).json({ error: 'Incorrect credentials' });
-          } else {
-            res.status(200).json({ User: `${newUser} created!` })
-          }
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            message: 'Error logging in!'
-        });
-    };
+//LOGIN
+router.post('/login', (req, res) => {
+    const {name, password} = req.body;
+    Users.findBy({ name })
+    .then(user => {
+        console.log(user);
+      if (user && bcrypt.compareSync(user.password, password)) {
+        res.status(200).json({ message: `Welcome ${user.name}!` });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: `Couldn't log in ${error}` });
+    });
+    // try {
+    //     const findUser = await User.findBy(name)
+    //     if (findUser && bcrypt.compareSync(password, findUser.password)) {
+    //         return res.status(401).json({ error: 'Incorrect credentials' });
+    //       } else {
+    //         res.status(200).json({ User: `${name} logged in!` })
+    //       }
+    // } catch (err) {
+    //     console.log(err);
+    //     res.status(500).json({
+    //         message: 'Error logging in!'
+    //     });
+    // };
 });
-
-router.post('/register', ValidateUser, async (req, res) => {
-    const credentials = { body } = req;
+//REGISTER
+router.post('/register', async (req, res) => {
+    const credentials = req.body;
     const hash = bcrypt.hashSync(credentials.password, 14);
     credentials.password = hash;
     try {
-        const newUser = await User.addUser(body);
-        res.status(200).json({ User: `${newUser} created!`})
+        const newUser = await Users.addUser(credentials);
+        res.status(200).json({ User: `${req.body.name} created!`})
     } catch (err) {
         console.log(err);
         res.status(500).json({
             message: 'Error creating new user!'
+        });
+    };
+});
+//GET ALL USERS
+router.get('/all', async (req, res) => {
+    try {
+        const allUsers = await Users.getAll();
+        res.status(200).json(allUsers);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Error getting all users!'
         });
     };
 });
